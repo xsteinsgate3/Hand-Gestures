b0f53bbecd24cdcd15d19c9c964954c5b05e3762
Roclay Rodrigues <roclayr@yahoo.com>
2023-03-16 19:12:57 -0300

    Added Notice
    Added a Notice to explain things and removed my attempt to numpy the test image.

--- hand_thing.py
+++ hand_thing.py
@@ -1,8 +1,17 @@
+'''
+    -- REQUIREMENT --
+    pip install mediapipe 
+
+    -- CURRENT ISSUE --
+    on Line 35, the second argument called "data" takes a numpy image.
+    However, it returns an error when i convert the 'hand.jpg' image as a numpy image
+    using the methods ive tried. 
+'''
+
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import imageio.v2 as imageio
-import numpy as np
 
 # Checking that packages were imported successfully.
 if mp and python and vision:
@@ -21,12 +30,6 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.IMAGE)
-
-# Load the image
-img = imageio.imread('hand.jpg')
-
-# Convert the image to a numpy array
-img_np = np.array(img)
 
 # Load the input image from a numpy array.
 mp_image = mp.Image(format=mp.ImageFormat.SRGB, data=img_np)
